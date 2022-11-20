e720237534158c2b29290434038a5e0d36417ef6
eunmin-kim <53runtime@gmail.com>
2022-11-20 23:11:26 +0900

    토글 버튼 만듬

--- app/src/main/java/com/example/no_alcohol/MainActivity.java
+++ app/src/main/java/com/example/no_alcohol/MainActivity.java
@@ -7,6 +7,9 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffffff")));
-        actionBar.setTitle(Html.fromHtml("<font color='#000099'>노알콜</font>"));
+        actionBar.setTitle(Html.fromHtml("<font color='#000099'>스탑 알콜</font>"));
+
+    }
+
+    public void stopAlcohol(View v) {
+        Button btnStopAlcohol = findViewById(R.id.stopAlcohol);
+        if (btnStopAlcohol.getText().equals("현재 금주 중 입니다!")) {
+            btnStopAlcohol.setText("금주 시작하기");
+        } else {
+            btnStopAlcohol.setText("현재 금주 중 입니다!");
+        }
     }
 }
