fd8ad344d5c3cdd2b086da19ce83579c841a5ae4
eunmin-kim <53runtime@gmail.com>
2022-11-20 22:55:11 +0900

    금주 레이아웃 만

--- app/src/main/java/com/example/no_alcohol/MainActivity.java
+++ app/src/main/java/com/example/no_alcohol/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.no_alcohol;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Html;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffffff")));
+        actionBar.setTitle(Html.fromHtml("<font color='#000099'>노알콜</font>"));
     }
 }
