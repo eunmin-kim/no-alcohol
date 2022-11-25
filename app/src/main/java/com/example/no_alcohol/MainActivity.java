10aa3a9e889bab362be41742b0e8fe855f20faa8
eunmin-kim <53runtime@gmail.com>
2022-11-25 11:28:30 +0900

    타이머 구현

--- app/src/main/java/com/example/no_alcohol/MainActivity.java
+++ app/src/main/java/com/example/no_alcohol/MainActivity.java
@@ -5,30 +5,62 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.text.BreakIterator;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+
+    Chronometer cmTimer;
+    Boolean resume = false;
+    long elapsedTime;
+    String TAG = "TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffffff")));
-        actionBar.setTitle(Html.fromHtml("<font color='#000099'>스탑 알콜</font>"));
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#378057")));
+        actionBar.setTitle(Html.fromHtml("<font color='#ffffff'>스탑 알콜</font>"));
 
     }
 
     public void stopAlcohol(View v) {
-        Button btnStopAlcohol = findViewById(R.id.stopAlcohol);
-        if (btnStopAlcohol.getText().equals("현재 금주 중 입니다!")) {
-            btnStopAlcohol.setText("금주 시작하기");
+        Button startBtn = findViewById(R.id.stopAlcohol);
+        cmTimer = (Chronometer) findViewById(R.id.cmTimer);
+        if (startBtn.getText().equals("현재 금주 중 입니다!")) {
+            startBtn.setText("금주 시작하기");
+
         } else {
-            btnStopAlcohol.setText("현재 금주 중 입니다!");
+            startBtn.setText("현재 금주 중 입니다!");
+            cmTimer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+                public void onChronometerTick(Chronometer arg0) {
+                    if (!resume) {
+                        long minutes = ((SystemClock.elapsedRealtime() - cmTimer.getBase())/1000) / 60;
+                        long seconds = ((SystemClock.elapsedRealtime() - cmTimer.getBase())/1000) % 60;
+                        elapsedTime = SystemClock.elapsedRealtime();
+                        Log.d(TAG, "onChronometerTick: " + minutes + " : " + seconds);
+                    } else {
+                        long minutes = ((elapsedTime - cmTimer.getBase())/1000) / 60;
+                        long seconds = ((elapsedTime - cmTimer.getBase())/1000) % 60;
+                        elapsedTime = elapsedTime + 1000;
+                        Log.d(TAG, "onChronometerTick: " + minutes + " : " + seconds);
+                    }
+                }
+            });
         }
     }
 }
